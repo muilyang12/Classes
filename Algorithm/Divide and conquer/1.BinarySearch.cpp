72ef839030a98b2d276b77a076faabdae4a56426
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2023-02-25 11:13:22 +0900

    Algorithm class - binary search

---
+++ Algorithm/Divide and conquer/1.BinarySearch.cpp
@@ -0,0 +1,27 @@
+// Binary Search
+// Time complexity: O(logn)
+
+int binarySearch(int arr, int left, int right, x) {
+    if (left > right) return -1;
+
+    int mid = (left + right) / 2;
+
+    if (arr[mid] == x) return mid;
+    else if (x < arr[mid]) binarySearchRecursive(arr, left, mid - 1, x);
+    else binarySearchRecursive(arr, mid + 1, right, x);
+}
+
+int binarySearchIteration(int[] arr, int n, int x) {
+    left = 0;
+    right = n - 1;
+
+    while (left <= right) {
+        int mid = (left + right) / 2;
+
+        if (arr[mid] == x) return mid;
+        else if (x < arr[mid]) right = mid - 1;
+        else left = mid + 1;
+    }
+
+    return -1;
+}
