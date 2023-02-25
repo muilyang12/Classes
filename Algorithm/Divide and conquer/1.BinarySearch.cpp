00cc49b8ea12add11ac6b7e31c5a6284a5fe1050
Muil Yang <ymi1210@midasit.com>
2023-02-25 12:34:01 +0900

    binary search 완성.

--- Algorithm/Divide and conquer/1.BinarySearch.cpp
+++ Algorithm/Divide and conquer/1.BinarySearch.cpp
@@ -1,7 +1,9 @@
 // Binary Search
 // Time complexity: O(logn)
 
-int binarySearch(int arr, int left, int right, x) {
+#include <iostream>
+
+int binarySearch(int arr[], int left, int right, int x) {
     if (left > right) return -1;
 
     int mid = (left + right) / 2;
@@ -11,17 +13,12 @@ int binarySearch(int arr, int left, int right, x) {
     else binarySearch(arr, mid + 1, right, x);
 }
 
-int binarySearchIteration(int[] arr, int n, int x) {
-    left = 0;
-    right = n - 1;
+int main() {
+	std::cout << "===== Start =====" << std::endl;
 
-    while (left <= right) {
-        int mid = (left + right) / 2;
-
-        if (arr[mid] == x) return mid;
-        else if (x < arr[mid]) right = mid - 1;
-        else left = mid + 1;
-    }
-
-    return -1;
+    int arr[] = { 10, 15, 30, 45, 50, 77, 80 };
+    std::cout << "Number " << 30 << " at " << binarySearch(arr, 0, 7, 30) << std::endl;
+    std::cout << "Number " << 80 << " at " << binarySearch(arr, 0, 7, 80) << std::endl;
+    std::cout << "Number " << 10 << " at " << binarySearch(arr, 0, 7, 10) << std::endl;
+    
 }
