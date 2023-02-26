801094c59f11cb6889761214b36ff68961c97435
Muil Yang <ymi1210@midasit.com>
2023-02-26 13:31:44 +0900

    quick sort 완성.

--- Algorithm/Divide and conquer/2.QuickSort.cpp
+++ Algorithm/Divide and conquer/2.QuickSort.cpp
@@ -1,34 +1,60 @@
 // Quick Sort
 // Time complexity: O(nlogn) (At worst: O(n^2))
 
-int quickSort(int[] arr, int n) {
-    if (n > 1) {
-       int pivot = partition(arr, n);
+#include <iostream>
+#include <vector>
 
-       quickSort(arr[0: pivot], pivot);
-       quickSort(arr[pivot + 1: n], n - pivot - 1);
-    }
+void swap(std::vector<int>& arr, int left, int right) {
+    int temp = arr[left];
+    arr[left] = arr[right];
+    arr[right] = temp;
 }
 
-int partition(int[] arr, int n) {
-    int left = 1;
-    int right = n - 1;
-
-    while (left < right) {
-        while (left < n && arr[left] < arr[0]) left++;
-        while (right > 0 && arr[right] > arr[0]) right--;
-
-        if (left < right) swap(arr[left], arr[right]);
-        else swap(arr[right], arr[0]);
+void printVector(std::vector<int>& arr) {
+    for (auto num : arr) {
+        std::cout << num << ' ';
     }
 
-    int newPivot = right;
+    std::cout << std::endl;
+}
+
+int partition(std::vector<int>& arr, int left, int right) {
+    int pivot = arr[left];
+    int start = left + 1;
+    int end = right;
+
+    while (start < end) {
+        while (start < right && arr[start] < pivot) start++;
+        while (end > left && arr[end] > pivot) end--;
+
+        if (start < end) swap(arr, start, end);
+        else swap(arr, end, left);
+    }
+
+    int newPivot = end;
 
     return newPivot;
 }
 
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+void quickSort(std::vector<int>& arr, int left, int right) {
+    if (left < right) {
+        int pivotIndex = partition(arr, left, right);
+
+        quickSort(arr, left, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, right);
+    }
+}
+
+int main() {
+    std::cout << "===== Start =====" << std::endl;
+
+    std::vector<int> numbers = { 5, 4, 3, 2, 1 };
+
+    std::cout << "Before sorted: ";
+    printVector(numbers);
+
+    quickSort(numbers, 0, numbers.size() - 1);
+
+    std::cout << "After sorted: ";
+    printVector(numbers);
 }
