758a03678c10ee0dbd2df5554937391643f2349b
Muil Yang <ymi1210@midasit.com>
2023-02-27 09:13:24 +0900

    merge sort 완성.

--- Algorithm/Divide and conquer/3.MergeSort.cpp
+++ Algorithm/Divide and conquer/3.MergeSort.cpp
@@ -1,43 +1,74 @@
 // Merge Sort
 // Time complexity: O(nlogn)
 
-void mergeSort(int[] arr, n) {
-    if (n > 1) {
-        int mid = n / 2;
+#include <iostream>
+#include <vector>
 
-        mergeSort(arr[0:mid], mid);
-        mergeSort(arr[mid:n], n - mid);
-
-        merge(arr[0:n], arr[0:mid], arr[mid:n], mid, n - mid);
+void printVector(std::vector<int>& arr) {
+    for (auto num : arr) {
+        std::cout << num << ' ';
     }
+
+    std::cout << std::endl;
 }
 
-void merge(int[] arr, int[] arr1, int[] arr2, n1, n2) {
-    int i = 0;
-    int j = 0;
+void merge(std::vector<int>& arr, int left, int mid, int right) {
+    std::vector<int> sortedArr(right - left + 1);
+
+    int i = left;
+    int j = mid + 1;
     int k = 0;
 
-    while (i < n1 && j < n2) {
-        if (arr1[i] < arr2[j]) {
-            arr[k] = arr1[i];
+    while (i <= mid && j <= right) {
+        if (arr[i] <= arr[j]) {
+            sortedArr[k] = arr[i];
             i++;
         } else {
-            arr[k] = arr2[j];
+            sortedArr[k] = arr[j];
             j++;
         }
 
         k++;
     }
 
-    while (i < n1) {
-        arr[k] = arr1[i];
+    while (i <= mid) {
+        sortedArr[k] = arr[i];
         i++;
         k++;
     }
 
-    while (j < n2) {
-        arr[k] = arr2[j];
+    while (j <= right) {
+        sortedArr[k] = arr[j];
         j++;
         k++;
     }
+
+    for (int m = left; m <= right; ++m) {
+        arr[m] = sortedArr[m - left];
+    }
+}
+
+void mergeSort(std::vector<int>& arr, int left, int right) {
+    if (left < right) {
+        int mid = (left + right) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr,  mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+
+int main() {
+    std::cout << "===== Start =====" << std::endl;
+
+    std::vector<int> numbers = { 5, 4, 3, 2, 1 };
+
+    std::cout << "Before sorted: ";
+    printVector(numbers);
+
+    mergeSort(numbers, 0, numbers.size() - 1);
+
+    std::cout << "After sorted: ";
+    printVector(numbers);
 }
